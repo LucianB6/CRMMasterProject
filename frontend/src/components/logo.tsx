7302746837ad5c389137f2999ce5c51b6bbaf5d2
LucianB6 <79158690+LucianB6@users.noreply.github.com>
2026-01-19 19:08:29 +0100

    frontend details

--- frontend/src/components/logo.tsx
+++ frontend/src/components/logo.tsx
@@ -2,15 +2,14 @@ import Link from "next/link";
 
 import { cn } from "../lib/utils";
 
-export function Logo({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+type LogoProps = Omit<React.ComponentPropsWithoutRef<"a">, "href">;
+
+export function Logo({ className, ...props }: LogoProps) {
   return (
     <Link
-      href="/"
+      href="/dashboard"
       className={cn(
-        "flex items-center justify-center gap-2 text-foreground",
+        "flex items-center justify-center gap-2",
         className
       )}
       {...props}
@@ -21,7 +20,6 @@ export function Logo({
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        className="text-primary"
       >
         <path
           d="M3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3"
